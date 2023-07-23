e5bf4491ce04c061125ad23e21ace8aace2f6502
mohamadreza <mohamadreza.ghasemi1383@gmail.com>
2023-07-23 13:51:34 +0430

    modified:   classes/dbh.class.php

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -1,17 +1,17 @@
 <?php 
 class Dbh {
-private $host = 'localhost';
-private $user = 'root';
-private $pwd = '';
-private $dbname = 'demo_sadra';
+    private $host = 'localhost';
+    private $user = 'root';
+    private $pwd = '';
+    private $dbname = 'demo_sadra';
 
-protected function connect(){
-    $con = new mysqli($this->host, $this->user, $this->pwd, $this->dbname);
-    if ($con -> connect_errno) {
-        echo "Failed to connect to MySQL: " . $con -> connect_error;
-        exit();
-      }
-    return $con;
-    }
+    protected function connect(){
+        $con = new mysqli($this->host, $this->user, $this->pwd, $this->dbname);
+        if ($con -> connect_errno) {
+            echo "Failed to connect to MySQL: " . $con -> connect_error;
+            exit();
+        }
+        return $con;
+        }
 }
 ?>
